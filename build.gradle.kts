7631e3c02cc9c9a4888f2afcf05915bcc5e11ec7
1037HLY <hs202027323@gmail.com>
2026-08-05 21:42:26 +0800

    chore: 简化根目录构建脚本

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,4 @@
-// 根目录构建脚本，所有插件已在settings.gradle.kts中管理
+// 根目录构建脚本 - 所有插件和依赖已在settings.gradle.kts和app/build.gradle.kts中管理
 tasks.register("clean", Delete::class) {
     delete(rootProject.buildDir)
 }

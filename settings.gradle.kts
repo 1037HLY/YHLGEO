601b64321eb1a175c2bebdefc7b08a59c988b92d
1037HLY <hs202027323@gmail.com>
2026-08-05 21:34:14 +0800

    fix: 调整仓库解析模式为PREFER_PROJECT

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,12 +5,16 @@ pluginManagement {
         gradlePluginPortal()
     }
 }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    // 将此模式改为 'PREFER_PROJECT'，允许项目中的仓库配置覆盖设置
+    repositoriesMode.set(RepositoriesMode.PREFER_PROJECT)
     repositories {
+        // 这里可以保留，但主要仓库在 allprojects 中定义
         google()
         mavenCentral()
     }
 }
+
 rootProject.name = "GeoSurveyToolbox"
 include(":app")
